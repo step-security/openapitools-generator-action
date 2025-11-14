f2a55b0e08a2954c012ecb21822be4bab065097a
amanstep <aman@stepsecurity.io>
2025-11-14 12:52:01 +0530

    fix: fixed shell injection vulnerability

--- entrypoint.py
+++ entrypoint.py
@@ -4,29 +4,37 @@ from os import getenv, getuid
 
 (_, generator, docker_repository, docker_image, generator_tag, openapi_file, openapi_url, config_file, template_dir, *args) = argv
 
-cmd = f"docker run -u {getuid()}:1001 --rm --workdir /github/workspace -v {getenv('GITHUB_WORKSPACE')}:/github/workspace"
-cmd = f"{cmd} {docker_repository}/{docker_image}:{generator_tag} generate"
-cmd = f"{cmd} -g {generator} -o /github/workspace/{generator}-client"
+cmd = [
+    "docker", "run",
+    "-u", f"{getuid()}:1001",
+    "--rm",
+    "--workdir", "/github/workspace",
+    "-v", f"{getenv('GITHUB_WORKSPACE')}:/github/workspace",
+    f"{docker_repository}/{docker_image}:{generator_tag}",
+    "generate",
+    "-g", generator,
+    "-o", f"/github/workspace/{generator}-client"
+]
 
 if openapi_url == "UNSET":
     if not openapi_file.startswith("/"):
         openapi_file = f"/github/workspace/{openapi_file}"
-    cmd = f"{cmd} -i {openapi_file}"
+    cmd.extend(["-i", openapi_file])
 else:
-    cmd = f"{cmd} -i {openapi_url}"
+    cmd.extend(["-i", openapi_url])
 
 if config_file != "UNSET":
     if not config_file.startswith("/"):
         config_file = f"/github/workspace/{config_file}"
-    cmd = f"{cmd} -c {config_file}"
+    cmd.extend(["-c", config_file])
 
 if template_dir != "UNSET":
     if not template_dir.startswith("/"):
         template_dir = f"/github/workspace/{template_dir}"
-    cmd = f"{cmd} -t {template_dir}"
+    cmd.extend(["-t", template_dir])
 
 if args:
-    cmd = f"{cmd} {' '.join(args)}"
+    cmd.extend(args)
 
 # Call the command and return the exit code
-exit(call(cmd, shell=True))
+exit(call(cmd))
